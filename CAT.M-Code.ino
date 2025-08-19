e4c93881fe019e78ce23d9956af843bbf0a12740
io5a <108933362+io5a@users.noreply.github.com>
2025-08-19 17:24:43 +0300

    Added the display functionality

--- CAT.M-Code.ino
+++ CAT.M-Code.ino
@@ -1,8 +1,16 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <Arduino_JSON.h>
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
 
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
 
+Adafruit_SSD1306 display1(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+#define I2C_ADDRESS 0x3C
 
 //const char* ssid = "CENSORED";
 const char* ssid = "Wokwi-GUEST";
@@ -20,6 +28,211 @@ double MainData[24]; // mix used array for hourly
 
 String weather;
 float weatherArr[3];
+
+const unsigned char cat_bitmap [] PROGMEM = {
+  0x00, 0xf8, 0x00, 0xfc, 0x00, 0x00, 0x01, 0x8c, 0x01, 0x86, 0x00, 0x00, 0x03, 0x06, 0x01, 0x32, 
+  0x00, 0x00, 0x02, 0x73, 0x02, 0x7a, 0x00, 0x00, 0x06, 0xf9, 0x02, 0xfb, 0x00, 0x00, 0x04, 0xfd, 
+  0x82, 0xf9, 0x00, 0x00, 0x05, 0xfc, 0xfe, 0xfd, 0x00, 0x00, 0x05, 0xfc, 0x54, 0xfd, 0x00, 0x00, 
+  0x05, 0xfc, 0x54, 0x7d, 0x00, 0x00, 0x04, 0xf0, 0x00, 0x1d, 0x00, 0x00, 0x04, 0x00, 0x00, 0x01, 
+  0x00, 0x00, 0x04, 0x00, 0x00, 0x01, 0x00, 0x00, 0x04, 0x00, 0x00, 0x01, 0x81, 0xfc, 0x04, 0x00, 
+  0x00, 0x00, 0x82, 0xaa, 0x04, 0x0e, 0x00, 0xe0, 0x82, 0xaa, 0x0c, 0x1f, 0x01, 0xf0, 0xc2, 0xaa, 
+  0x08, 0x0e, 0x00, 0xe0, 0x42, 0x02, 0x08, 0x00, 0x30, 0x00, 0x42, 0x02, 0x08, 0x00, 0x30, 0x00, 
+  0x42, 0x02, 0x08, 0x00, 0x00, 0x00, 0x42, 0x02, 0x08, 0x00, 0x00, 0x00, 0x42, 0x02, 0x08, 0x02, 
+  0x31, 0x00, 0x42, 0x02, 0x08, 0x03, 0x7b, 0x00, 0x42, 0x02, 0x0c, 0x01, 0xce, 0x00, 0x42, 0x02, 
+  0x04, 0x00, 0x00, 0x00, 0x82, 0x02, 0x06, 0x00, 0x00, 0x00, 0x82, 0x02, 0x03, 0x00, 0x00, 0x00, 
+  0x82, 0x02, 0x01, 0x00, 0x00, 0x03, 0x82, 0x06, 0x03, 0x80, 0x00, 0x0e, 0xc6, 0x04, 0x06, 0x60, 
+  0x00, 0x00, 0x4c, 0x04, 0x04, 0x00, 0x00, 0x00, 0x58, 0x04, 0x0c, 0x00, 0x00, 0x00, 0x70, 0x0c, 
+  0x18, 0x00, 0x00, 0x00, 0x00, 0x08, 0x10, 0x00, 0x00, 0x00, 0x00, 0x18, 0x30, 0x00, 0x00, 0x00, 
+  0x00, 0x30, 0x60, 0x00, 0x00, 0x00, 0x00, 0x60, 0x40, 0xc0, 0x00, 0x00, 0x00, 0xc0, 0xc0, 0x70, 
+  0x00, 0x00, 0x07, 0x80, 0x80, 0x1c, 0x00, 0x00, 0x1c, 0x00, 0x80, 0x04, 0x00, 0x00, 0x20, 0x00, 
+  0x40, 0x24, 0x00, 0x00, 0x20, 0x00, 0x60, 0x94, 0x00, 0x00, 0x20, 0x00, 0x32, 0x4c, 0x00, 0x00, 
+  0x20, 0x00, 0x19, 0x3c, 0x00, 0x00, 0x30, 0x00, 0x08, 0x98, 0x00, 0x00, 0x18, 0x00, 0x1c, 0xe0, 
+  0x00, 0x00, 0x0c, 0x00, 0x37, 0x98, 0x00, 0x07, 0xe4, 0x00, 0x20, 0x0c, 0x00, 0x1c, 0x34, 0x00, 
+  0x20, 0x04, 0x00, 0x30, 0x16, 0x00, 0x45, 0x44, 0x00, 0x65, 0x52, 0x00, 0x45, 0x44, 0x00, 0x45, 
+  0x52, 0x00, 0x45, 0x44, 0x00, 0x45, 0x5a, 0x00, 0x40, 0x04, 0x00, 0x40, 0x0a, 0x00, 0x63, 0x84, 
+  0x00, 0x40, 0x0e, 0x00, 0x23, 0x88, 0x00, 0x43, 0x8c, 0x00, 0x33, 0x88, 0x00, 0x43, 0x8c, 0x00, 
+  0x18, 0x7f, 0xff, 0xc3, 0x98, 0x00, 0x0f, 0xc0, 0x00, 0xe0, 0x30, 0x00, 0x00, 0x00, 0x00, 0x3f, 
+  0xe0, 0x00
+};
+
+void DisplayClock(int x,int y){
+  //7,7
+  display1.drawCircle(x, y, 5, SSD1306_WHITE);
+  display1.drawLine(x, y, x, y-3, SSD1306_WHITE);
+  display1.drawLine(x, y, x+2, y, SSD1306_WHITE);
+
+}
+void DisplayInTherm(int x,int y){
+    // 7,19
+    //x4,y22 -> x4,y16
+    display1.drawLine(x-3,y+3,x-3,y-3,SSD1306_WHITE);
+    //x8,y20 -> x8,y15
+    display1.drawLine(x+1,y+1,x+1,y-4,SSD1306_WHITE);
+    //x11
+    display1.drawLine(x+4,y+1,x+4,y-4,SSD1306_WHITE);
+    //x7,y21->x7,y23
+    display1.drawLine(x,y+2,x,y+4,SSD1306_WHITE);
+    //x12
+    display1.drawLine(x+5,y+2,x+5,y+4,SSD1306_WHITE);
+    //x8,y24->x11,y24
+    display1.drawLine(x+1,y+5,x+4,y+5,SSD1306_WHITE);
+    //x9,y14->x10,y14
+    display1.drawLine(x+2, y-5,x+3,y-5, SSD1306_WHITE);
+    //x10,y16
+    display1.drawPixel(x+3, y-3, SSD1306_WHITE);
+    //x10,y18
+    display1.drawPixel(x+3, y-1, SSD1306_WHITE);
+    //x10,y20
+    display1.drawPixel(x+3, y+1,SSD1306_WHITE);
+}
+
+void DisplayOutTherm(int x,int y){
+    //x7,y31
+    display1.drawLine(x+1,y+1,x+1,y-4,SSD1306_WHITE);
+    //x11
+    display1.drawLine(x+4,y+1,x+4,y-4,SSD1306_WHITE);
+    //x7,y21->x7,y23
+    display1.drawLine(x,y+2,x,y+4,SSD1306_WHITE);
+    //x12
+    display1.drawLine(x+5,y+2,x+5,y+4,SSD1306_WHITE);
+    //x8,y24->x11,y24
+    display1.drawLine(x+1,y+5,x+4,y+5,SSD1306_WHITE);
+    //x9,y14->x10,y14
+    display1.drawLine(x+2, y-5,x+3,y-5, SSD1306_WHITE);
+    //x10,y16
+    display1.drawPixel(x+3, y-3, SSD1306_WHITE);
+    //x10,y18
+    display1.drawPixel(x+3, y-1, SSD1306_WHITE);
+    //x10,y20
+    display1.drawPixel(x+3, y+1,SSD1306_WHITE);
+    //x2,y29->x2,y33
+    display1.drawLine(x-5,y-2,x-5,y+2,SSD1306_WHITE);
+    //x5,y29->x5,y33
+    display1.drawLine(x-2,y-2,x-2,y+2,SSD1306_WHITE);
+    //x3,y28->x4,y28
+    display1.drawLine(x-4,y-3,x-3,y-3,SSD1306_WHITE);
+    //x3,y34->x4,y34
+    display1.drawLine(x-4,y+3,x-3,y+3,SSD1306_WHITE);
+}
+
+void DisplayMoon(int x,int y){
+    //x7,y43
+
+    display1.drawCircle(x,y,5,SSD1306_WHITE);
+    //x4,y41
+    display1.drawRect(x-3, y-2, 2, 2, SSD1306_WHITE);
+    //x8,y43
+    display1.drawRect(x+1,y,3,2,SSD1306_WHITE);
+    //x9,y41
+    display1.drawRect(x+2,y-2,2,2,SSD1306_WHITE);
+    //x5,y46->x6,y45
+    display1.drawLine(x-2,y+3,x-1,y+2,SSD1306_WHITE);
+    //x5,x45
+    display1.drawPixel(x-2,y+2,SSD1306_WHITE);
+
+}
+
+void DisplaySun(int x,int y){
+  //x7,y55
+  
+  //x3,y59->x11,y51
+  display1.drawLine(x-4,y+4,x+4,y-4,SSD1306_WHITE);
+  //x3,y51->x11,y59
+  display1.drawLine(x-4,y-4,x+4,y+4,SSD1306_WHITE);
+  //x7,y50->x7,y60
+  display1.drawLine(x,y-5,x,y+5,SSD1306_WHITE);
+  //x2,y55->x12,y55
+  display1.drawLine(x-5,y,x+5,y,SSD1306_WHITE);
+
+  display1.fillCircle(x,y,3,SSD1306_WHITE);
+  display1.fillCircle(x,y,2,SSD1306_BLACK);
+
+}
+
+void DisplayCloud(int x,int y){
+  //x96,y5
+
+
+  //x91,y10->x101,y10
+  display1.drawLine(x-5,y+5,x+5,y+5,SSD1306_WHITE);
+  //x91,y9->x91,y7
+  display1.drawLine(x-5,y+4,x-5,y+2,SSD1306_WHITE);
+  //x92,y5->x92,y6
+  display1.drawLine(x-4,y,x-4,y+1,SSD1306_WHITE);
+  //x93,y4->x93,y5
+  display1.drawLine(x-3,y-1,x-3,y,SSD1306_WHITE);
+  //x94,y4->x97,y4
+  display1.drawLine(x-2,y-1,x+1,y-1,SSD1306_WHITE);
+  //x98,y6->x98,y5
+  display1.drawLine(x+2,y+1,x+2,y,SSD1306_WHITE);
+  //x99,y6->x99,y7
+  display1.drawLine(x+3,y+1,x+3,y+2,SSD1306_WHITE);
+  //x100,y7->x101,y7
+  display1.drawLine(x+4,y+2,x+5,y+2,SSD1306_WHITE);
+  //x101,y9->x101,y8
+  display1.drawLine(x+5,y+4,x+5,y+3,SSD1306_WHITE);
+  //x93,y7->x93,y8
+  display1.drawLine(x-3,y+2,x-3,y+3,SSD1306_WHITE);
+  //x94,y6
+  display1.drawPixel(x-2,y+1,SSD1306_WHITE);
+}
+
+void NextHourTherm(int x,int y){
+    //96 17
+    //therm
+    display1.drawLine(x+1,y+1,x+1,y-4,SSD1306_WHITE);
+    display1.drawLine(x+4,y+1,x+4,y-4,SSD1306_WHITE);
+    display1.drawLine(x,y+2,x,y+4,SSD1306_WHITE);
+    display1.drawLine(x+5,y+2,x+5,y+4,SSD1306_WHITE);
+    display1.drawLine(x+1,y+5,x+4,y+5,SSD1306_WHITE);
+    display1.drawLine(x+2, y-5,x+3,y-5, SSD1306_WHITE);
+    display1.drawPixel(x+3, y-3, SSD1306_WHITE);
+    display1.drawPixel(x+3, y-1, SSD1306_WHITE);
+    display1.drawPixel(x+3, y+1,SSD1306_WHITE);
+    //n
+    //x91,y12->x91,y22
+    display1.drawLine(x-5,y-5,x-5,y+5,SSD1306_WHITE);
+    //x94,y13->x94,y22
+    display1.drawLine(x-2,y-5,x-2,y+5,SSD1306_WHITE);
+    //x92,y16->x92,y17;
+    display1.drawLine(x-4,y-1,x-4,y,SSD1306_WHITE);
+    //x93,y17->x93,y18;
+    display1.drawLine(x-3,y,x-3,y+1,SSD1306_WHITE);
+
+}
+
+void DisplayCATM(int x,int y){
+  //border
+  //89,44
+  //x89,y44->x127,y44
+  display1.drawLine(x,y,x+38,y,SSD1306_WHITE);
+  //x89,y45->x89,y63
+  display1.drawLine(x,y+1,x,y+19,SSD1306_WHITE);
+  //text
+  display1.setTextSize(1);
+  display1.setTextColor(SSD1306_WHITE);
+  display1.setCursor(x+2,y+2);
+  display1.print("CAT");  
+  display1.setCursor(x+21,y+2);
+  display1.drawPixel(x+19,y+8,SSD1306_WHITE);
+  display1.print("M");  
+  //119,50
+  display1.setCursor(x+32,y+2);
+  display1.print("1");
+  //v
+  display1.drawLine(x+28,y+6,x+28,y+7,SSD1306_WHITE);
+  display1.drawLine(x+29,y+7,x+29,y+8,SSD1306_WHITE);
+  display1.drawLine(x+30,y+6,x+30,y+7,SSD1306_WHITE);
+
+  //by ioa
+  display1.setCursor(x+2,y+12);
+  display1.print("by ioa");
+}
+
+void DisplayCat(int x,int y){
+  display1.drawBitmap(x, y, cat_bitmap, 47, 59, SSD1306_WHITE);
+}
+
 
 void setup(){
 
@@ -41,6 +254,32 @@ void setup(){
     Serial.print("Local ESP32 IP: ");
     Serial.println(WiFi.localIP());
   // END CONNECT WIFI
+
+  // Initialize single display
+  Wire.begin();  // to remove the args, just for the simulation
+  delay(200);
+  
+  // Initialize Display (Speed/GPS switchable)
+  Serial.println("Initializing Display at 0x3C...");
+  if (display1.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println("✓ Display OK");
+    display1.clearDisplay();
+    display1.setTextColor(SSD1306_WHITE);
+    DisplayClock(7,7);
+    DisplayInTherm(7,19);
+    DisplayOutTherm(7,31);
+    DisplayMoon(7,43);
+    DisplaySun(7,55);
+    DisplayCloud(96,5);
+    NextHourTherm(96,17);
+    DisplayCATM(89,44);
+    DisplayCat(39,3);
+    display1.display();
+    display1.display();
+    delay(2000);
+  } else {
+    Serial.println("✗ Display failed");
+  }
 }
 
 void loop() {
@@ -146,61 +385,13 @@ void parseWeather(String jsonString,int ChooseNr, double& ReturnNr, String& Retu
   }
 }
 
-  /*
-  // --- Top-level fields ---
-  double latitude = double(root["latitude"]);
-  double longitude = double(root["longitude"]);
-  String timezone = (const char*) root["timezone"];
-
-  Serial.println("Top-level:");
-  Serial.print("Latitude: "); Serial.println(latitude);
-  Serial.print("Longitude: "); Serial.println(longitude);
-  Serial.print("Timezone: "); Serial.println(timezone);
-
-  // --- Current weather ---
-  JSONVar current = root["current"];
-  String currentTime = (const char*) current["time"];
-  double tempNow = double(current["temperature_2m"]);
-  double precipNow = double(current["precipitation"]);
-
-  Serial.println("\nCurrent:");
-  Serial.print("Time: "); Serial.println(currentTime);
-  Serial.print("Temp: "); Serial.println(tempNow);
-  Serial.print("Precipitation: "); Serial.println(precipNow);
-
-  // --- Hourly data ---
-  JSONVar hourly = root["hourly"];
-  JSONVar hourlyTimes = hourly["time"];
-  JSONVar hourlyTemps = hourly["temperature_2m"];
-
-  Serial.println("\nHourly:");
-  for (int i = 0; i < hourlyTimes.length(); i++) {
-    Serial.print((const char*) hourlyTimes[i]);
-    Serial.print(" - ");
-    Serial.println(double(hourlyTemps[i]));
-  }
-
-  // --- Daily data ---
-  JSONVar daily = root["daily"];
-  String sunrise = (const char*) daily["sunrise"][0];
-  String sunset = (const char*) daily["sunset"][0];
-
-  Serial.println("\nDaily:");
-  Serial.print("Sunrise: "); Serial.println(sunrise);
-  Serial.print("Sunset: "); Serial.println(sunset);
-  */
 
 String httpGETRequest(const char* serverName) {
   WiFiClient client;
   HTTPClient http;
     
-  // Your Domain name with URL path or IP address with path
   http.begin(client, weatherserver);
   
-  // If you need Node-RED/server authentication, insert user and password below
-  //http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
-  
-  // Send HTTP POST request
   int httpResponseCode = http.GET();
   
   String payload = "{}"; 
@@ -214,7 +405,6 @@ String httpGETRequest(const char* serverName) {
     Serial.print("Error code: ");
     Serial.println(httpResponseCode);
   }
-  // Free resources
   http.end();
 
   return payload;
